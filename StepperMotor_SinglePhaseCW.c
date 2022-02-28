646adbeda8eb2cdb944bcf01c0917849dec08f9d
Nidhi Zala <nidhizala2307@gmail.com>
2022-02-28 11:52:50 +0530

    Add files via upload

---
+++ StepperMotor_SinglePhaseCW.c
@@ -0,0 +1,36 @@
+/*
+ * DcMotor.c
+ *
+ * Created: 2/28/2022 10:58:53 AM
+ * Author : HP
+ */ 
+#include<stdio.h>
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include<util/delay.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+	DDRC=0xFF;
+    while (1) 
+    {
+		//A1 on
+		PORTC=0x08; 
+		_delay_ms(1000);
+		
+		//B1 on
+		PORTC=0x04;
+		_delay_ms(1000);
+		
+		//A2 on
+		PORTC=0x02;
+		_delay_ms(1000);
+		
+		//B2 on
+		PORTC=0x01;
+		_delay_ms(1000);
+		
+    }
+}
+
